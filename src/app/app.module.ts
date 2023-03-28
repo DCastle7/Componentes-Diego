00c9d21e47d23dc8f89c17e9e3b27d1146ca20f1
Diego Castillo <diegcast@gmail.com>
2023-03-27 21:03:43 -0300

    Commit de Ejercicio completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContentComponent,
+    ToolbarComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule
